63df48cb6df5a4271d05dfe0db6a52340f6b2f2f
menisadi <menisadi@gmail.com>
2024-02-20 15:07:48 +0200

    bounce sideqay

--- main.lua
+++ main.lua
@@ -24,16 +24,30 @@ function love.update(dt)
 	-- bead.speed.x = bead.speed.x + bead.acceleration.x
 
 	bead.y = bead.y + bead.speed.y * dt
-	-- bead.x = bead.x + bead.speed.x * dt
+	bead.x = bead.x + bead.speed.x * dt
 
+	-- bead got to the bottom of the screen -> appear at the top
 	if bead.y > window_height then
 		bead.y = 0
+		bead.x = window_width / 2
+		bead.speed.x = 0
+		bead.speed.y = 200
 	end
 
-	if bead.y + bead.radius >= peg.y - peg.radius then
-		bead.y = peg.y - peg.radius - bead.radius
-		bead.speed.y = -bead.speed.y * 0.8
+	local function dist(p1, p2)
+		local dist_square = math.pow(p1.x - p2.x, 2) + math.pow(p1.y - p2.y, 2)
+		return math.sqrt(dist_square)
 	end
+
+	-- colision -> bounce
+	if dist(bead, peg) <= bead.radius + peg.radius then
+		bead.y = peg.y - peg.radius - bead.radius
+		local side = 2 * math.random(2) - 3
+		bead.speed.x = bead.speed.y * 0.4 * side
+		bead.speed.y = -bead.speed.y * 0.4
+	end
+	-- if bead.y + bead.radius >= peg.y - peg.radius then
+	-- end
 end
 
 function love.draw()
